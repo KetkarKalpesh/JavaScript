7caf66c464ae856921dff7ea09f5c03f1d39adb4
Kalpesh Ketkar <106992078+KetkarKalpesh@users.noreply.github.com>
2023-03-23 15:47:24 +0530

    Add files via upload

---
+++ ReminderOperator.js
@@ -0,0 +1,18 @@
+/*The remainder (%) operator returns the remainder left over when one operand is divided by a second operand. 
+It always takes the sign of the dividend.*/
+
+
+console.log(10 % 2)
+//Expected output: 0
+
+console.log(13 % 5);
+// Expected output: 3
+
+console.log(-13 % 5);
+// Expected output: -3
+
+console.log(4 % 2);
+// Expected output: 0
+
+console.log(-4 % 2);
+// Expected output: -0
