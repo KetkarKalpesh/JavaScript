0f7ba8431ff1a5f7201cb48cd2d954a6e3c71f47
Kalpesh Ketkar <106992078+KetkarKalpesh@users.noreply.github.com>
2023-03-29 00:57:45 +0530

    Update Sum_PositiveNumbers.js

--- Sum_PositiveNumbers.js
+++ Sum_PositiveNumbers.js
@@ -1,5 +1,20 @@
 /*Sum of All Positive Numbers*/
 
+/*Using For Loop*/
+const number = parseInt(prompt('Enter a positive integer: '));
+
+let sum = 0;
+
+// looping from i = 1 to number
+// in each iteration, i is increased by 1
+for (let i = 1; i <= number; i++) {
+    sum += i;
+}
+
+console.log('The sum of natural numbers:', sum);
+
+
+/*Using While Loop*/
 const number = parseInt(prompt('Enter a Positive Number: '));
 
 let sum = 0, i = 1;
